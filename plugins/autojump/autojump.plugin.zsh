4cb95721aecda629359abc6cbfe4c6c0983a4669
Jacob Atzen <jacob@incremental.dk>
2011-09-26 12:05:12 +0200

    Update autojump plugin to handle Debian

--- plugins/autojump/autojump.plugin.zsh
+++ plugins/autojump/autojump.plugin.zsh
@@ -1,3 +1,11 @@
-if [ -f `brew --prefix`/etc/autojump ]; then
-  . `brew --prefix`/etc/autojump
+# debian flavour
+if [ -f /usr/share/autojump/autojump.zsh ]; then
+  . /usr/share/autojump/autojump.zsh
+fi
+
+# mac os x flavour - not tested
+if (`command -v brew >/dev/null`); then
+  if [ -f `brew --prefix`/etc/autojump ]; then
+    . `brew --prefix`/etc/autojump
+  fi
 fi
